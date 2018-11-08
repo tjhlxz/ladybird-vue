5b439519abc96eafc2f2f3648fbf2fda95b173c3
Coward <949942046@qq.com>
2018-11-08 12:27:12 +0800

    update form_detail css

--- src/pages/Supervise/Supervise_form_detail.vue
+++ src/pages/Supervise/Supervise_form_detail.vue
@@ -66,54 +66,54 @@
                                         </div>
                                         <div class="num5">
                                             <div class="number8">
-                                                <div v-for="item in date_before">{{item}}</div>
+                                                <div v-if="date_before.length!==0" v-for="item in date_before">{{item}}</div>
                                                 <div v-for="item3 in before">{{item3}}</div>
                                             </div>
                                             <div class="number9">
-                                                <div v-for="item1 in date_after">{{item1}}</div>
-                                                <div v-for="item4 in before">{{item4}}</div>
+                                            <div v-if="date_before.length!==0" v-for="item1 in date_after">{{item1}}</div>
+                                            <div v-for="item4 in before">{{item4}}</div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="number1 num3">原&nbsp;&nbsp;因</div>
-                                    <div style="padding:5px 5px;width:750px;text-align: left;font-size: 18px;">{{content.form_reason}}</div>
+                                    <div style="padding:5px 8px;width:16.5cm;text-align: left;font-size: 14pt;">{{content.form_reason}}</div>
                                 </div>
                                 <div class="row number3">
-                                    <div class="number2">教研室意见</div>
-                                    <div class="agree">同意</div>
-                                    <div style="margin-top:50px;margin-left:100px;width:220px;">
-                                        <div>教研室主任：{{leader[0]}}</div>
-                                        <div>{{datetime[1]}}</div>
+                                        <div class="number2">教研室意见</div>
+                                        <div class="agree">同意</div>
+                                        <div style="margin-top:50px;margin-left:200px;width:220px;">
+                                            <div>教研室主任：{{leader[0]}}</div>
+                                            <div>{{datetime[1]}}</div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="row number3">
-                                    <div class="number2">教学单位意见</div>
-                                    <div class="agree">同意</div>
-                                    <div style="margin-top:50px;margin-left:100px;width:220px;">
-                                        <div>主管教学领导：{{leader[1]}}</div>
-                                        <div>{{datetime[2]}}</div>
+                                    <div class="row number3">
+                                        <div class="number2">教学单位意见</div>
+                                        <div class="agree">同意</div>
+                                        <div style="margin-top:50px;margin-left:200px;width:220px;">
+                                            <div>主管教学领导：{{leader[1]}}</div>
+                                            <div>{{datetime[2]}}</div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="row number3">
-                                    <div class="number2">教务处意见</div>
-                                    <div class="agree">同意</div>
-                                    <div style="margin-top:50px;margin-left:100px;width:220px;">
-                                        <div>教务处副处长：{{leader[2]}}</div>
-                                        <div>{{datetime[3]}}</div>
+                                    <div class="row number3">
+                                        <div class="number2">教务处意见</div>
+                                        <div class="agree">同意</div>
+                                        <div style="margin-top:50px;margin-left:200px;width:220px;">
+                                            <div>教务处副处长：{{leader[2]}}</div>
+                                            <div>{{datetime[3]}}</div>
+                                        </div>
                                     </div>
-                                </div>
                                 <div class="row last">
                                     <div class="number1 beizhu">备&nbsp;&nbsp;注</div>
                                     <div class="content">
                                         <div class="item">1.调串类型：调串申请时教师课表不会发生永久变化，此时无附件。<br></div>
-                                        <div class="item">&nbsp;&nbsp;&nbsp;A.临时代课：调整前后时间保持不变填写，明确填写代课教师；<br></div>
-                                        <div class="item">&nbsp;&nbsp;&nbsp;B.临时顺延：调整前后时间地点按二级学院教务科实际可行填写，代课教师可不填；<br></div>
-                                        <div class="item">&nbsp;&nbsp;&nbsp;C.临时调串：若干次调串时间地点按二级学院教务科实际可行填写，代课教师可不填；<br></div>
+                                        <div class="item">&nbsp;A.临时代课：调整前后时间保持不变填写，明确填写代课教师；<br></div>
+                                        <div class="item">&nbsp;B.临时顺延：调整前后时间地点按二级学院教务科实际可行填写，代课教师可不填；<br></div>
+                                        <div class="item">&nbsp;C.临时调串：若干次调串时间地点按二级学院教务科实际可行填写，代课教师可不填；<br></div>
                                         <div class="item">2.变更类型：变更申请时教师课表将永久变化，教务处教务科变更后新课表详见附件。<br></div>
-                                        <div class="item">&nbsp;&nbsp;&nbsp;A.教师永久性变更：申请整门课程的任课教师永久性变更。<br></div>
-                                        <div class="item">&nbsp;&nbsp;&nbsp;B.时间地点永久性变更：申请整门课的上课时间地点永久性变更。<br></div>
+                                        <div class="item">&nbsp;A.教师永久性变更：申请整门课程的任课教师永久性变更。<br></div>
+                                        <div class="item">&nbsp;B.时间地点永久性变更：申请整门课的上课时间地点永久性变更。<br></div>
                                     </div>
                                 </div>
                             </div>
@@ -151,9 +151,15 @@ export default {
         });
         this.axios.get(_global.baseUrl + 'find_form?form_id=' + this.$route.query.id).then(body => {
             loading.modal('close');
+            
             this.content = body.data.data;
+            if(this.content.form_before_adjust!==''){
             this.date_before=this.content.form_before_adjust.split(',');
             this.date_after=this.content.form_later_adjust.split(',');
+            }
+            else{
+                this.date_before=[];
+            }
             for(var i=0;i<4-this.date_before.length;i++){
                 this.before.push('第 周 星期 第 节');
             }
@@ -183,130 +189,143 @@ export default {
 </script>
 <style media="print" type="text/css">
 .heikeji{
-
-    font-size: 25px;
-    letter-spacing:4px;
+    font-size: 16pt;
+    letter-spacing:11px;
+}
+.print_box{
+    width: 100%;
+}
+.print_page{
+    width: 21cm;
+    height:29.7cm;
 }
 .print{
-    width: 900px;
+    width: 21cm;
+    height:29.7;
     font-family:SimSun;
-    font-size: 20px;
-    font-weight: 700;
-    padding:100px 23px;
+    font-size: 14pt;
+    padding:1.5cm 1cm;
 }
 .form{
-    font-size: 40px;
-    font-weight: 700;
-    font-family: SimHei;
+    font-family:SimHei;
+    height:1.5cm;
+    line-height: 1.5cm;
+    font-size: 28pt;
 }
 .kuang{
-    width: 850px;
-}
-.type{
-    display: flex;
-    flex-direction: row;
-    margin-top: 20px;
+    width: 19cm;
+    height:23cm;
+    border:1pt solid #000;
 }
 .row{
-    width: 850px;
+    width: 19cm;
     display: flex;
     flex-direction: row;
-    border-bottom: 1px solid #000;
-}
-.last{
-    border-bottom: none;
-    height: 250px;
-
-}
-.item{
-    line-height: 28px;
-    text-align: left;
-    font-size: 18px;
-    margin-bottom: 3px;
-}
-.beizhu{
-    height:250px;
-    line-height: 250px;
-    text-align: center;
-}
-.content{
-    font-family: SimHei;
-    width: 800px;
-    padding: 20px 10px;
-}
-.biandong{
-    height: 150px;
+    border-bottom: 1pt solid #000;
 }
 .number1{
-    width: 100px;
-    border-right: 1px solid #000;
-}
-.number2{
-    height: 120px;
-    line-height: 120px;
-    text-align: center;
-    width: 180px;
-    border-right: 1px solid #000;
-}
-.number3{
-    height: 120px;
-}
-.num3{
-    height: 130px;
-    line-height: 130px;
-    text-align: center;
-
-}
-.number4{
-    display: flex;
-    flex-direction: row;
-    border-bottom: 1px solid #000;
-    height: 30px;
-}
-.number5{
-    width: 250px;
-    border-right: 1px solid #000;
-}
-.number6{
-    width: 130px;
-    border-right: 1px solid #000;
-}
-.number7{
-    width:330px;
-}
-.number8{
-    width:380px;
-    border-right: 1px solid #000;
-}
-.number9{
-    width:330px;
+    width:2.5cm;
+    border-right: 1pt solid #000;
 }
 .num1{
-    height: 60px;
-    line-height: 60px;
-    text-align: center;
+    height:1.1cm;
+    line-height: 1.1cm;
+}
+.number5{
+    width: 5.2cm;
+    border-right: 1pt solid #000;
+}
+.number6{
+    width: 3cm;
+    border-right: 1pt solid #000;
+}
+.number7{
+    width: 8.2cm;
+}
+.biandong{
+    height:3.3cm;
+    line-height: 3.3cm
 }
 .num2{
     display: flex;
     flex-direction: column;
     align-items: center;
 }
-.agree{
-    margin: 40px 130px;
-    font-size: 28px;
-}
-.num4{
-    height:150px;
-    display: flex;
-    flex-direction: column;
-    width: 750px;
-    font-size: 18px;
-}
-.num5{
+.type{
+    height:0.8cm;
+    line-height: 0.8cm;
+    margin-top: 0.5cm;
     display: flex;
     flex-direction: row;
-    width: 750px;
-    height: 120px;
+}
+.num4{
+    width: 16.5cm;
+}
+.number4{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 1cm;
+    line-height: 1cm;
+    text-align: center;
+    border-bottom:1pt solid #000;
+}
+.num3{
+    height: 3cm;
+    line-height: 3cm;
+}
+.number8{
+    width:8.2cm;
+    border-right: 1pt solid #000;
+    display: flex;
+    flex-direction: column;
+}
+.number9{
+    width: 8.2cm;
+}
+.num5{
+    height: 2.3cm;
+    padding:0;
+    display: flex;
+    flex-direction: row;
+}
+.number8 div{
+    height: 0.56cm;
+    line-height: 0.56cm;
+    font-size: 12pt;
+}
+.number9 div{
+    height: 0.56cm;
+    line-height: 0.56cm;
+    font-size: 12pt;
+}
+.number2{
+    width: 4.5cm;
+    height: 3cm;
+    line-height: 3cm;
+    border-right: 1pt solid #000;
+}
+.agree{
+    font-size: 24pt;
+    margin-top: 1cm;
+    margin-left: 1cm;
+}
+.beizhu{
+    height:5.3cm;
+    line-height: 5.3cm;
+    text-align: center;
+}
+.last{
+    border-bottom: 0;
+    height: 5.3cm;
+}
+.content{
+    padding: 0.2cm 0.1cm;
+    font-family:SimHei;
+    font-size: 12pt;
+    height:5.3cm;
+    line-height: 0.7cm;
+    text-align: left;
 }
 @page{
     size:  auto;   /* auto is the initial value */
